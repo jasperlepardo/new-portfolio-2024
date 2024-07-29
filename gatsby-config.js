5ca055314570458975ff66702df3e4f513d37d34
Jasper Lepardo <jsprlprd@gmail.com>
2024-07-30 05:05:33 +0800

    rebase

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                spaceId: "54w41zdktalp",
+                accessToken: "QKUISQo6T22fGMZ09XxRNq1KF492laFAJyg7xSHz1SY",
+            }
+        },
+        "gatsby-plugin-image"
+    ]
+}
